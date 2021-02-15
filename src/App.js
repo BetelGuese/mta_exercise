750d40b8c79eca0f3286b66131f0534111b491db
Akos Foldesi <gilikter.akos@gilgam.es>
2021-02-15 09:19:53 +0100

    Material UI dark theme installed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const darkTheme = createMuiTheme({
+		palette: {
+		  type: 'dark',
+		}
+	  });
+	return (
+		<ThemeProvider theme={darkTheme}>
+			<div className="App">
+				<header className="App-header">
+
+				</header>
+			</div>
+		</ThemeProvider>
+	);
 }
 
 export default App;
